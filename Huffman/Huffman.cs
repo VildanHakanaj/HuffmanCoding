773c3b43d53c111c9a328415a7ac98645f32fccf
Vildan Hakanaj <vildanhakanaj@gmail.com>
2018-10-25 21:26:35 -0400

    Added all the data fields and the constructor for the Huffman class

--- Huffman/Huffman.cs
+++ Huffman/Huffman.cs
@@ -8,5 +8,18 @@ namespace Huffman
 {
     class Huffman
     {
+
+        #region Class field Declaration
+        private const int FREQ_SIZE = 256;                                     //Ascii table size
+        private string Message;                                                //Store the message
+        private PriorityQueue<Node> pq = new PriorityQueue<Node>(35);          //Priority queue
+        private Dictionary<char, string> D = new Dictionary<char, string>();   //Hash Table
+        private int[] freq = new int[FREQ_SIZE];                               //Frequency Array
+        #endregion
+
+        public Huffman(string Message)
+        {
+            this.Message = Message;
+        }
     }
 }
