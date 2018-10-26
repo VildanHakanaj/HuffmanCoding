5419c3a0a64c4f908adcc1d2c398d0fe391ce927
Vildan Hakanaj <vildanhakanaj@gmail.com>
2018-10-25 21:22:16 -0400

    Finished the Node Class - Added all the properties - Added the construtor - Added and implemented the IComparable method

--- Huffman/Node.cs
+++ Huffman/Node.cs
@@ -8,5 +8,17 @@ namespace Huffman
 {
     class Node
     {
+        public char Letter { get; set; }
+        public int Frequency { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node(char Letter, int Frequency, Node Left, Node Right)
+        {
+            this.Letter = Letter;
+            this.Frequency = Frequency;
+            this.Left = Left;
+            this.Right = Right;
+        }
     }
 }
